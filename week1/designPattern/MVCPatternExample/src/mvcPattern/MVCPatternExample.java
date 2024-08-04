088115b2a8d355bd049d029dff2e0a2fc7e8c202
Madulika Prabusankar <131234604+madulika-prabu@users.noreply.github.com>
2024-08-04 21:42:16 +0530

    Add files via upload

---
+++ week1/designPattern/MVCPatternExample/src/mvcPattern/MVCPatternExample.java
@@ -0,0 +1,26 @@
+package mvcPattern;
+
+public class MVCPatternExample {
+    public static void main(String[] args) {
+        Student model = retrieveStudentFromDatabase();
+
+        StudentView view = new StudentView();
+
+        StudentController controller = new StudentController(model, view);
+
+        controller.updateView();
+
+        controller.setStudentName("John");
+        controller.setStudentGrade("A");
+
+        controller.updateView();
+    }
+
+    private static Student retrieveStudentFromDatabase() {
+        Student student = new Student();
+        student.setName("Robert");
+        student.setId("10");
+        student.setGrade("B");
+        return student;
+    }
+}
