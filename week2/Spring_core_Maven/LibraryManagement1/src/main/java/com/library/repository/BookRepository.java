9d06d514c47dcfd01add622524f4c8093cbae53e
Madulika Prabusankar <131234604+madulika-prabu@users.noreply.github.com>
2024-08-12 18:55:16 +0530

    Add files via upload

---
+++ week2/Spring_core_Maven/LibraryManagement1/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package com.library.repository;
+
+public class BookRepository {
+
+    public void saveBook(String bookName) {
+     
+        System.out.println("BookRepository: Saving book - " + bookName);
+    }
+}
+
+
