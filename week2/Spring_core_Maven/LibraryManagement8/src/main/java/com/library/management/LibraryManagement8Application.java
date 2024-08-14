743d8e040dd5d7078cafcebcf46b16affef8e398
Madulika Prabusankar <131234604+madulika-prabu@users.noreply.github.com>
2024-08-14 17:51:10 +0530

    Add files via upload

---
+++ week2/Spring_core_Maven/LibraryManagement8/src/main/java/com/library/management/LibraryManagement8Application.java
@@ -0,0 +1,14 @@
+package com.library.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class LibraryManagement8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagement8Application.class, args);
+	}
+
+}
