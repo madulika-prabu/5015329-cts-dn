1227758e2a0263acd002c8a6e3fc81641cfa9315
Madulika Prabusankar <131234604+madulika-prabu@users.noreply.github.com>
2024-08-14 16:59:32 +0530

    Add files via upload

---
+++ week2/Spring_core_Maven/LibraryManagement6/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,28 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    // Constructor-based dependency injection
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    // Setter-based dependency injection
+    @Autowired
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void addBook(String bookName) {
+        System.out.println("BookService: Adding book - " + bookName);
+        bookRepository.saveBook(bookName);
+    }
+}
