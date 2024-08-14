fdfd4739730b0c7c76d6c891a534a2b4c3117074
Madulika Prabusankar <131234604+madulika-prabu@users.noreply.github.com>
2024-08-14 17:47:12 +0530

    Add files via upload

---
+++ week2/Spring_core_Maven/LibraryManagement8/src/main/java/com/library/management/controller/BookController.java
@@ -0,0 +1,43 @@
+package com.library.management.controller;
+
+import com.library.management.model.Book;
+import com.library.management.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Book> getBookById(@PathVariable Long id) {
+        return bookRepository.findById(id);
+    }
+
+    @PostMapping
+    public Book createBook(@RequestBody Book book) {
+        return bookRepository.save(book);
+    }
+
+    @PutMapping("/{id}")
+    public Book updateBook(@PathVariable Long id, @RequestBody Book book) {
+        book.setId(id);
+        return bookRepository.save(book);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable Long id) {
+        bookRepository.deleteById(id);
+    }
+}
