8ceec61495ed0c16ad6afff6506cbacb762208f6
Madulika Prabusankar <131234604+madulika-prabu@users.noreply.github.com>
2024-08-21 13:48:31 +0530

    Add files via upload

---
+++ week3/EmployeeManagementSystemWeek/src/main/java/com/example/employeemanagementsystem/controller/EmployeeController.java
@@ -0,0 +1,31 @@
+package com.example.employeemanagementsystem.controller;
+
+import com.example.employeemanagementsystem.model.Employee;
+import com.example.employeemanagementsystem.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeService.saveEmployee(employee);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteEmployee(@PathVariable Long id) {
+        employeeService.deleteEmployee(id);
+    }
+}
