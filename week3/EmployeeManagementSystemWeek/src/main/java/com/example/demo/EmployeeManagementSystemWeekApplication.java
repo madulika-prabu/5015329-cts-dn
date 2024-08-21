bb49248797143cbff348dc264d52f8bef330bbce
Madulika Prabusankar <131234604+madulika-prabu@users.noreply.github.com>
2024-08-21 13:46:21 +0530

    Add files via upload

---
+++ week3/EmployeeManagementSystemWeek/src/main/java/com/example/demo/EmployeeManagementSystemWeekApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementSystemWeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementSystemWeekApplication.class, args);
+	}
+
+}
