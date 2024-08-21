12a71c333559d4287cf7076846de7356af570db5
Madulika Prabusankar <131234604+madulika-prabu@users.noreply.github.com>
2024-08-21 14:45:48 +0530

    Delete week3/EmployeeManagementSystemWeek/src directory

--- week3/EmployeeManagementSystemWeek/src/main/java/com/example/employeemanagementsystem/repository/EmpolyeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.employeemanagementsystem.repository;
-
-import com.example.employeemanagementsystem.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmpolyeeRepository extends JpaRepository<Employee, Long> {
-}
